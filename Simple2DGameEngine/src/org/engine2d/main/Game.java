0c133c65d3c53e206e6b5aa84386617c53846555
HenrikSydow <henrik.alexander.sydow@gmail.com>
2021-07-16 04:02:12 +0200

    Second Commit

--- Simple2DGameEngine/src/org/engine2d/main/Game.java
+++ Simple2DGameEngine/src/org/engine2d/main/Game.java
@@ -1,22 +1,29 @@
 package org.engine2d.main;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Game extends JPanel {
+public class Game extends JPanel implements Runnable{
 
 	public static final int GAME_WIDTH = 500, GAME_HEIGHT = 500;
 	public static final String GAME_NAME = "";
+	
+	private Thread gameThread;
+	
+	private boolean running = false;
 
 	public Game() {
 		this.setDoubleBuffered(true);
 		this.setPreferredSize(new Dimension(GAME_WIDTH, GAME_HEIGHT));
 
 		initGameFrame();
+		start();
 	}
 	
+	// sets up the JFrame containing the Game-Object:
 	private void initGameFrame() {
 		JFrame gameFrame = new JFrame(GAME_NAME);
 		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -26,6 +33,41 @@ public class Game extends JPanel {
 		gameFrame.setLocationRelativeTo(null);
 		gameFrame.setResizable(false);
 		gameFrame.setVisible(true);
+	}
+	
+	// Gameloop:
+	public void run() {
+		while (running) {
+			update();
+			repaint();
+		}
+	}
+	
+	// updates the game-object:
+	private void update() {
+		
+	}
+	
+	// renders / paints when triggered by the repaint()-method:
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+	}
+	
+	// starts the gameloop:
+	public void start() {
+		running = true;
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+	
+	// stops the gameloop:
+	public void stop() {
+		running = false;
+		try {
+			gameThread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
